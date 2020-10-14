36cb188c4f37fa8c76ff89096ba32fb10729ab79
berellevy <berellevy@gmail.com>
2020-10-14 14:38:26 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
+import { Container } from 'react-bootstrap'
 import logo from './logo.svg';
 import './App.css';
+import Home from './Pages/Home';
+import NavBar from './Components/NavBar';
+import Footer from './Components/Footer';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container bg="blue">
+        <NavBar />
+        <Home/>
+        <Footer />
+      </Container>
     </div>
   );
 }
