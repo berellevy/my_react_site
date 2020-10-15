e565331af8936f383d72af1fba8d9600cd3ef582
berellevy <berellevy@gmail.com>
2020-10-14 23:28:48 -0400

    Updates

---
+++ precache-manifest.cc9d373e800e63ac36232118f9f013c4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bbe59ea69d6bac35dc52568abacbfb6",
+    "url": "/www.berellevy.com/index.html"
+  },
+  {
+    "revision": "6c4bf179f3d0fc249754",
+    "url": "/www.berellevy.com/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "46266cbb17821a71f61d",
+    "url": "/www.berellevy.com/static/css/main.db268c6f.chunk.css"
+  },
+  {
+    "revision": "6c4bf179f3d0fc249754",
+    "url": "/www.berellevy.com/static/js/2.9429bbd4.chunk.js"
+  },
+  {
+    "revision": "451ffe720aa8add9f554b8ed54913322",
+    "url": "/www.berellevy.com/static/js/2.9429bbd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46266cbb17821a71f61d",
+    "url": "/www.berellevy.com/static/js/main.88ae91e8.chunk.js"
+  },
+  {
+    "revision": "54a0bfc69fd9cf0fd155",
+    "url": "/www.berellevy.com/static/js/runtime-main.c7f02deb.js"
+  },
+  {
+    "revision": "0505e3e63bf4c401da41c4abb624f8fb",
+    "url": "/www.berellevy.com/static/media/Berel_Levy_Resume.0505e3e6.docx"
+  },
+  {
+    "revision": "25e2689beb4207e6e5a4c6d4710a6f26",
+    "url": "/www.berellevy.com/static/media/round-profile.25e2689b.png"
+  }
+]);
