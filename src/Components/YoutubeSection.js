0fbc82366f885ac4dcbd0f2c132a7e2f7fe4aa1c
berellevy <berellevy@gmail.com>
2020-10-20 21:43:59 -0400

    I think I'm getting somewhere with the youtube cards

--- src/Components/YoutubeSection.js
+++ src/Components/YoutubeSection.js
@@ -1,29 +1,50 @@
 import React from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
-import ReactPlayer from 'react-player'
+import { Container, Row } from 'react-bootstrap'
 
 
 const YoutubeSection = () => {
   return (
     <div style={{background: "#707990"}}>
       <Container>
+        <h1 className="text-center">Check out some of my youtube clips!</h1>
         <Row 
-          className="row-cols-1 row-cols-sm-2"
+          className="row-cols-1 row-cols-sm-2 p-4"
         >
-          <Col>
-            <ReactPlayer 
-              controls="true"
-              width="100%"
-              url="https://youtu.be/pA0NRgoMUy0"
-            />
-          </Col>
-          <Col>
-            <ReactPlayer 
-              controls="true"
-              width="100%"
-              url="https://youtu.be/pA0NRgoMUy0"
-            />
-          </Col>
+          <div className="col p-3">
+            <div className="card">
+              <div className="embed-responsive embed-responsive-16by9 p-5">
+                <iframe
+                  className="p-1"
+                  title="unique" 
+                  width="560" 
+                  height="315" 
+                  src="https://www.youtube.com/embed/pA0NRgoMUy0" 
+                  frameborder="0" 
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                  allowfullscreen
+                ></iframe>
+              </div> 
+              <div class="card-body">
+              <h5 class="card-title">FauxMeaux Short Demo</h5>
+                <p class="card-text">
+                  It's a unique take on ads, counterintuitive but compelling.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col p-3">
+            <div className="embed-responsive embed-responsive-16by9">
+              <iframe
+                title="unique" 
+                width="560" 
+                height="315" 
+                src="https://www.youtube.com/embed/pA0NRgoMUy0" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen
+              ></iframe>
+            </div>
+          </div>
         </Row>
       </Container>
     </div>
