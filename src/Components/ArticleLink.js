905540b55df9a2cec54cdc663cc89e8b2ae309c6
berellevy <berellevy@gmail.com>
2024-05-02 18:22:16 -0400

    match bg for now

--- src/Components/ArticleLink.js
+++ src/Components/ArticleLink.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Card, CardBody, CardTitle, CardLink, CardText} from 'react-bootstrap'
 export const ArticleLink = ({title, text, href}) => {
     return (
-        <Card>
+        <Card style={{background: "#e6dbd4"}}>
             <CardBody>
                 <CardTitle>{title}</CardTitle>
                 <CardText>{text}</CardText>
