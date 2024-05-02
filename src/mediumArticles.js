0c136e1db2ae76ca63b225bc7cc482730dfa63d5
berellevy <berellevy@gmail.com>
2024-05-02 18:22:07 -0400

    make it easily updatable

---
+++ src/mediumArticles.js
@@ -0,0 +1,19 @@
+const articles = [
+    {
+        title: "Pause/Play YouTube with Your Keyboard!",
+        text: "Let's make a keyboard shortcut that you can click in any active window that will pause your YouTube tutorial.",
+        href: "https://medium.com/@berellevy/pause-play-youtube-with-your-keyboard-b747029f9f4a",
+    },
+    {
+        title: "Closure in JavaScript",
+        text: "what is closure? There's a short answer and a long answer.",
+        href: "https://medium.com/@berellevy/closure-in-javascript-d04fe11d81c3",
+    },
+    {
+        title: "Introducing FauxMeaux 2.0 (patent pending)",
+        text: "A new concept in social media advertising that will benefit all parties involved.",
+        href: "https://medium.com/@berellevy/introducing-fauxmeaux-2-0-patent-pending-6b1a14584ab3",
+    },
+]
+
+export default articles
