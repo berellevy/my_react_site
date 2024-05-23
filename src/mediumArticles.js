2a447ee8d1d52e00ed44ac958ad459ffc3e266ec
Berel Levy <berellevy@gmail.com>
2024-05-23 12:57:40 -0400

    added article

--- src/mediumArticles.js
+++ src/mediumArticles.js
@@ -1,5 +1,10 @@
 const articles = [
     {
+        title: "HTMX + 3rd Party Libraries The Easy Way.",
+        text: "Easily activate HTMX on all elements added to the DOM by 3rd party libraries etc. with one simple code snippet.",
+        href: "https://blog.stackademic.com/htmx-3rd-party-libraries-the-easy-way-6b83601e8b95",
+    }
+    {
         title: "Out-Of-This-World Django DX",
         text: "How to set up the Jupyter Notebook VSCode extension in Django.",
         href: "https://blog.stackademic.com/out-of-this-world-django-dx-e26612a38fd6",
