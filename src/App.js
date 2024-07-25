10f97b13eccecf1fe6a27a2ef5c31be85b2b9399
berellevy <berellevy@gmail.com>
2024-07-25 10:51:26 -0400

    log medium rss

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 import Home from './Pages/Home';
 import NavBar from './Components/NavBar';
 import Footer from './Components/Footer';
+import { getMediumArticles } from './mediumArticles';
 
 
 
 
 function App() {
+  getMediumArticles().then(console.log)
   return (
     <div className="App Site">
         <NavBar />
