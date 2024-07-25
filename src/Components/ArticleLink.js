cbaad3f64939566261c2cf03d3399071ebe31e85
berellevy <berellevy@gmail.com>
2024-07-25 12:48:59 -0400

    add date

--- src/Components/ArticleLink.js
+++ src/Components/ArticleLink.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import {Card, CardBody, CardTitle, CardLink, CardText} from 'react-bootstrap'
-export const ArticleLink = ({title, text, href}) => {
-    return (
-        <Card style={{background: "#e6dbd4"}}>
-            <CardBody>
-                <CardTitle>{title}</CardTitle>
-                <CardText>{text}</CardText>
-                <CardLink href={href}>Read More...</CardLink>
-            </CardBody>
-        </Card>
-    )
+export const ArticleLink = ({title, snippet, href, pubDate}) => {
+  return (
+    <Card style={{background: "#e6dbd4"}}>
+      <CardBody>
+        <CardTitle>{title}</CardTitle>
+        <CardText>{snippet}</CardText>
+        <CardText>{new Date(pubDate).toLocaleDateString()}</CardText>
+        <CardLink href={href}>Read More...</CardLink>
+      </CardBody>
+    </Card>
+  )
 }
