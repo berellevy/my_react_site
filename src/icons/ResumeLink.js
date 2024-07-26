eaa6fb9d3d63c54e4e3e7fd036f7d449a72fc997
berellevy <berellevy@gmail.com>
2024-07-26 10:59:29 -0400

    try again from public

--- src/icons/ResumeLink.js
+++ src/icons/ResumeLink.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import resume from "../assets/BerelLevy_Resume.pdf"
 
+
 const ResumeLink = () => {
   return (
-    <a href={resume} className='text-light' download="BerelLevy_Resume.pdf">
+    <a href="./BerelLevy_Resume.pdf" className='text-light' download="BerelLevy_Resume.pdf">
       Resume
     </a>
   )
