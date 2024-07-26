5585cbca4babd9da5ca2f6d8d9116a4f6baf2299
berellevy <berellevy@gmail.com>
2024-07-26 10:51:21 -0400

    fix: wrong link

--- src/icons/ResumeLink.js
+++ src/icons/ResumeLink.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import resume from "../assets/BerelLevy_Resume.pdf"
 
 const ResumeLink = () => {
   return (
-    <a href={"./BerelLevy_Resume.pdf"} className='text-light' download="BerelLevy_Resume.pdf">
+    <a href={resume} className='text-light' download="BerelLevy_Resume.pdf">
       Resume
     </a>
   )
