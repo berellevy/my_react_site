ccdb500dfba19be0fc362b55e4538b84e4817a61
berellevy <berellevy@gmail.com>
2024-07-26 11:00:41 -0400

    remove resume from assets

--- src/icons/ResumeLink.js
+++ src/icons/ResumeLink.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import resume from "../assets/BerelLevy_Resume.pdf"
-
-
 const ResumeLink = () => {
   return (
-    <a href="./BerelLevy_Resume.pdf" className='text-light' download="BerelLevy_Resume.pdf">
+    <a
+      href="./BerelLevy_Resume.pdf"
+      className="text-light"
+      download="BerelLevy_Resume.pdf"
+    >
       Resume
     </a>
   )
 }
-
 
 export default ResumeLink
