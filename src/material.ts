51b1729793fdd9e182f6b5616aaddc58308c3379
berellevy <berellevy@gmail.com>
2024-07-29 13:29:37 -0400

    create theme

---
+++ src/material.ts
@@ -0,0 +1,20 @@
+import { createTheme } from "@mui/material";
+
+
+
+export const theme = createTheme({
+//   typography: {
+//     allVariants: {
+//         color: teal[800]
+//     },
+//   },
+palette: {
+    mode: 'light',
+    primary: {
+      main: '#4b4e6d',
+    },
+    secondary: {
+      main: '#e6dbd4',
+    },
+  },
+})
