d417f7b1c5e45151cec87b4d36195039caed2a1e
berellevy <berellevy@gmail.com>
2024-11-27 10:56:02 -0500

    fix broken a tags

--- src/components/ArticleLink.js
+++ src/components/ArticleLink.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import {Card, CardBody, CardTitle, CardLink, CardText} from 'react-bootstrap'
-export const ArticleLink = ({title, snippet, href, pubDate}) => {
+export const ArticleLink = ({title, snippet, link, pubDate}) => {
   return (
     <Card style={{background: "light"}}>
       <CardBody>
         <CardTitle>{title}</CardTitle>
         <CardText>{snippet}</CardText>
         <CardText>{new Date(pubDate).toLocaleDateString()}</CardText>
-        <CardLink href={href}>Read More...</CardLink>
+        <CardLink target='_blank' href={link}>Read More...</CardLink>
       </CardBody>
     </Card>
   )
