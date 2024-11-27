570301ce45b4902832c2bce91abfbe6ef84b629a
berellevy <berellevy@gmail.com>
2024-11-27 10:55:40 -0500

    new resume

--- src/icons/ResumeLink.js
+++ src/icons/ResumeLink.js
@@ -2,9 +2,9 @@ import React from 'react'
 const ResumeLink = () => {
   return (
     <a
-      href="./BerelLevy_Resume.pdf"
+      href="./Berel_Levy_Resume.pdf"
       className="text-light"
-      download="BerelLevy_Resume.pdf"
+      download="Berel_Levy_Resume.pdf"
     >
       Resume
     </a>
